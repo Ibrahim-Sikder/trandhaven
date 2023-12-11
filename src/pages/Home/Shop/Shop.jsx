3dcf9e8ca7fe640c3a7df1b3a4854213cd7aba74
Ibrahim-Sikder <ibrahimsikder5033@gmail.com>
2023-12-11 08:59:14 +0600

    shop-section-added

--- src/pages/Home/Shop/Shop.jsx
+++ src/pages/Home/Shop/Shop.jsx
@@ -2,30 +2,87 @@
 import './Shop.css'
 import {StarBorder, } from '@mui/icons-material';
 import fruit from '../../../../public/assets/fruit.jpg'
+import fruit2 from '../../../../public/assets/fruit2.jpg'
+import fruit3 from '../../../../public/assets/fruit3.jpg'
+import fruit4 from '../../../../public/assets/fruit4.jpg'
+import fruit5 from '../../../../public/assets/fruit5.jpg'
+import fruit10 from '../../../../public/assets/shop.png'
 
 const Shop = () => {
+    const shopData = [
+        {
+            id:1,
+            name: "Apple",
+            desc: '"Fruitful Living: Nourishing Your Health, One Bite at a Time"',
+            price: 399,
+            img: fruit,
+        },
+        {
+            id:1,
+            name: "Apple",
+            desc: '"Fruitful Living: Nourishing Your Health, One Bite at a Time"',
+            price: 399,
+            img: fruit2,
+        },
+        {
+            id:1,
+            name: "Apple",
+            desc: '"Fruitful Living: Nourishing Your Health, One Bite at a Time"',
+            price: 399,
+            img: fruit3,
+        },
+        {
+            id:1,
+            name: "Apple",
+            desc: '"Fruitful Living: Nourishing Your Health, One Bite at a Time"',
+            price: 399,
+            img: fruit4,
+        },
+        {
+            id:1,
+            name: "Apple",
+            desc: '"Fruitful Living: Nourishing Your Health, One Bite at a Time"',
+            price: 399,
+            img: fruit5,
+        },
+    ]
     return (
-        <div>
-            <div className="shopWrap">
+        <div className='shopWrap'>
+            <div>
                 <h3 className='text-2xl font-bold mb-5'>Nature's Bounty, Delivered Fresh</h3>
-                <div className="shopCard">
-                   <div className="imgWrap">
-                   <img src={fruit} alt="" />
-                   </div>
-                   <div className='shopContent'>
-                    <h3 className="text-2xl font-bold">Apples</h3>
-                    <p className='my-1'>"Fruitful Living: Nourishing Your Health, One Bite at a Time"</p>
-                    <div >
-                         <StarBorder className='reviewIcon'/>
-                         <StarBorder className='reviewIcon'/>
-                         <StarBorder className='reviewIcon'/>
-                         <StarBorder className='reviewIcon'/>
-                         <StarBorder className='reviewIcon'/>
-                    </div>
-                    <span className='block my-1'>BDT 399৳</span>
+              <div className="shopCardsWrap">
+              {
+                shopData.map(shop=> <div key={shop.id} className="shopCard">
+                <div className="imgWrap">
+                <img src={shop.img} alt="" />
+                </div>
+                <div className='shopContent'>
+                 <h3 className="text-2xl font-bold">{shop.name}</h3>
+                 <p className='my-1'>{shop.desc} </p>
+                 <div >
+                      <StarBorder className='reviewIcon'/>
+                      <StarBorder className='reviewIcon'/>
+                      <StarBorder className='reviewIcon'/>
+                      <StarBorder className='reviewIcon'/>
+                      <StarBorder className='reviewIcon'/>
+                 </div>
+                 <span className='block my-2'>BDT <b>{shop.price}৳</b></span>
 
-                    <button className='addToCardBtn'>Add to cart</button>
-                   </div>
+                 <button className='addToCardBtn'>Add to cart</button>
+                </div>
+             </div>)
+               }
+              </div>
+            </div>
+            <div className="organicFruits my-16">
+                <div className="leftSide">
+                    <img src={fruit10} alt="" />
+                </div>
+                <div className="rightSideContent">
+                    <h3 className="text-3xl font-bold mt-2">"PrimePicks: Where Every Vegetable is Verified Perfection"</h3>
+                    <h6 className="text-xl font-bold">VeggieVista: Explore the Green Market Online</h6>
+                    <p className='my-2'>Our commitment to quality is unwavering. With our freshness guarantee, you can trust that every vegetable you receive is handpicked" </p>
+                    <button className='shopBtn'>Shop Now </button>
                 </div>
             </div>
         </div>
---
+++ src/shared/ShopBtn/ShopBtn.jsx
@@ -0,0 +1,10 @@
+
+const ShopBtn = () => {
+    return (
+        <div>
+            <button className="shopBtn">Shop Now </button>
+        </div>
+    );
+};
+
+export default ShopBtn;
